480818a41d48c9884e915bb155460ff9398d4780
seyhaheng2 <seyhaheng2@gmail.com>
2015-03-17 08:30:20 +0700

    first commit

---
+++ config/initializers/assets.rb
@@ -0,0 +1,14 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.precompile += %w( icons-000000.png )
+Rails.application.config.assets.precompile += %w( icons-ffffff.png )
+Rails.application.config.assets.precompile += %w( icons-000000@2x.png )
+Rails.application.config.assets.precompile += %w( icons-ffffff@2x.png )
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
